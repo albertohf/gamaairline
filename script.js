5297f2f188daa3cdaf4afb8028e323053a21fc33
Alberto Fernandes <albertohf91@gmail.com>
2021-07-16 03:03:37 -0300

    att js

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ let campoEmail = document.getElementById('email')
 
 form.addEventListener('submit', e => {
   // alerta o valor do campo
-  alert(campoNome.value)
-  alert(campoEmail.value)
+  if (campoNome.value === '' || campoEmail.value === '') {
+    alert('Preencher o Formulário!!!')
+  } else {
+    localStorage.setItem('name', campoNome.value)
+    localStorage.setItem('email', campoEmail.value)
+
+    console.log(localStorage)
+  }
 
   // impede o envio do form
   e.preventDefault()
